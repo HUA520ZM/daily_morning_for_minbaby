da09001be6b3d5dc41d0eeddfdc5c1f220eba91d
HUA520ZM <117702255+HUA520ZM@users.noreply.github.com>
2023-03-12 16:57:17 +0800

    Update main-new.py

--- new/main-new.py
+++ new/main-new.py
@@ -60,7 +60,7 @@ def send_message(to_user, access_token, city_name, weather, max_temperature, min
     month = localtime().tm_mon
     day = localtime().tm_mday
     today = datetime.date.today()
-    week = week_list[today.isoweekday()]
+    week = week_list[0]
     # 获取在一起的日子的日期格式
     love_year = int(config.love_date.split("-")[0])
     love_month = int(config.love_date.split("-")[1])
