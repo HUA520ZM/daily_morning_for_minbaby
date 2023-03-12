cedc67615a1bb0f472c6f75c49a7a269018cfe13
HUA520ZM <117702255+HUA520ZM@users.noreply.github.com>
2023-03-12 16:12:14 +0800

    Update config.py

--- new/config.py
+++ new/config.py
@@ -1,22 +1,22 @@
 # 公众号配置
 # 公众号appId
-app_id = "wx176d5c56df579e6b"
+app_id = "wxc2ddbb28335701bc"
 # 公众号appSecret
-app_secret = "5df4816360a0ab5eb86cda66bfec854e"
+app_secret = "1d898e3693a7205accfcead9543ca4d9"
 # 模板消息id
-template_id = "ZoifqEmlmiIFK_Tz3F63Imdws6qHTK-ODnAn_foESmU"
+template_id = "KZqIGawIkmiGVyYVcsfK5v9lDSLtZJ6ZkUrpIqE-R8Y"
 # 接收公众号消息的微信号
 # 这是我的
-users = "odicq61cUoRuFevQZJiiZ34hla2A,odicq61cUoRuFevQZJiiZ34hla2A"
+users = "o48Cd5-DwhG-5B09448-FPTRO3zs,o48Cd51hdyYZq3n2xK-RpkQuKOXA"
 # 这是宝宝的
 # user = ""
 
 # 信息配置
 # 所在省份
-province = "湖南"
+province = "湖北"
 # 所在城市
-city = "长沙"
+city = "武汉"
 # 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
-birthday = "2022-7-22"
+birthday = "2022-4-1"
 # 在一起的日子，格式同上
-love_date = "2019-1-14"
+love_date = "2022-09-13"
