a93ddc17b445bf8bfc806f5299e849fa3b390b4c
HUA520ZM <117702255+HUA520ZM@users.noreply.github.com>
2023-04-06 20:39:18 +0800

    Update config.py

--- new/config.py
+++ new/config.py
@@ -4,12 +4,11 @@ app_id = "wxc2ddbb28335701bc"
 # 公众号appSecret
 app_secret = "1d898e3693a7205accfcead9543ca4d9"
 # 模板消息id
-template_id = "M1vpZUL7WT4iVVVRWzY_f4DkE23AH6shwuOVnmUgrx0"
+template_id = "4QueHAJOA1oMJqiaDbOxinqAAY1qqZCoIxMUqnFDdQY"
 # 接收公众号消息的微信号
 # 这是我的
 users = "o48Cd5-DwhG-5B09448-FPTRO3zs,o48Cd51hdyYZq3n2xK-RpkQuKOXA"
-# 这是宝宝的
-#user = ""
+
 
 # 信息配置
 # 所在省份
@@ -17,7 +16,7 @@ province = "湖北"
 # 所在城市
 city = "武汉市黄陂区"
 # 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
-birthday = "1999-4-1"
+birthday = "1999-5-19"
 birthday2 = "1999-11-11"
 # 在一起的日子，格式同上
 love_date = "2022-09-13"
