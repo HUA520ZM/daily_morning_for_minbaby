318fa9a9383fb478c7097a32a1f8d06221414037
HUA520ZM <117702255+HUA520ZM@users.noreply.github.com>
2023-05-17 16:02:57 +0800

    config.py

--- new/config.py
+++ new/config.py
@@ -4,7 +4,7 @@ app_id = "wx107c8e135c2c4ff4"
 # 公众号appSecret
 app_secret = "2942ef3baf3d7e57aa7649545cd6bae3"
 # 模板消息id
-template_id = "SqjySajNAPF8-QH8tfRFHKyUU-oC_1k4e9iHPoE9TBI"
+template_id = "uGgIOsDoQFvJVbA81N6P9XDiRTFAfrchRinSzzxVOxo"
 # 接收公众号消息的微信号
 # 这是我的
 users = "ofbRU6JZ5_-goJlMsQ3yBF881ypI"
