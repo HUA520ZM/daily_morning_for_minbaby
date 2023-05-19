55a055b59228b9f80475cc739ca03aac9e09efda
HUA520ZM <117702255+HUA520ZM@users.noreply.github.com>
2023-05-19 10:36:32 +0800

    config.py

--- new/config.py
+++ new/config.py
@@ -7,7 +7,7 @@ app_secret = "2942ef3baf3d7e57aa7649545cd6bae3"
 template_id = "i3HfPnkSPS2sKmZ3p2iNwgkkQ2uMwq04FDKpMv5JXx4"
 # 接收公众号消息的微信号
 # 这是我的
-users = "ofbRU6JZ5_-goJlMsQ3yBF881ypI"
+users = "ofbRU6MWFTdevQUu6W70chl_MMTA,ofbRU6JZ5_-goJlMsQ3yBF881ypI"
 
 
 # 信息配置
